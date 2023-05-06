8f09313db1dafe3e45eb7c5f0728fe47ed2b0101
Talal El Zeini <talalelzeini@gmail.com>
2023-05-06 01:25:48 -0700

    made winning message show up

--- SudokuUI.java
+++ SudokuUI.java
@@ -1,3 +1,4 @@
+
 import java.util.EventListener;
 import java.awt.*;
 import java.awt.event.*;
@@ -9,11 +10,25 @@ public class SudokuUI extends Sudoku implements EventListener
 
     private static CellNode[][] UIBoard = new CellNode[9][9];
 
-
     public static void main(String[] args) {
 
-        int sum = 0;
+        JFrame frame = new JFrame("Sudoku Grid");
+        JPanel panel = new JPanel(new BorderLayout());
+        JPanel gridPanel = new JPanel(new GridLayout(9, 9));
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        JPanel winningPanel = new JPanel(new FlowLayout());
+        
+        // Buttons
+        JButton easyButton = new JButton("Easy");
+        JButton mediumButton = new JButton("Medium");
+        JButton hardButton = new JButton("Hard");
 
+        buttonPanel.add(easyButton);
+        buttonPanel.add(mediumButton);
+        buttonPanel.add(hardButton);
+        
+        int sum = 0;
+        
         Sudoku userBoard = new Sudoku();
         Sudoku sourceBoard = new Sudoku();
 
@@ -25,28 +40,44 @@ public class SudokuUI extends Sudoku implements EventListener
 
         userBoard.copyBoard(sourceBoard);
 
-        userBoard.removeNumbers(3); // 81 - 21 = 60 (considered hard)
-
-        System.out.println("\nUnsolved Board");
-        int[] unsolvedBoardOneDimensional = userBoard.printBoard();
+        userBoard.removeNumbers(1);
 
 
-//         System.out.println(unsolvedBoard[0]);
+        easyButton.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    System.out.println("Easy");
 
+                }
+            }
+        );
 
+        mediumButton.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    System.out.println("Medium");
+                }
+            }
+        );
 
-        JFrame frame = new JFrame("Sudoku Grid");
-        JPanel panel = new JPanel(new BorderLayout());
-        JPanel gridPanel = new JPanel(new GridLayout(9, 9));
-        JPanel buttonPanel = new JPanel(new FlowLayout());
+        hardButton.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    System.out.println("Hard");
 
+                }
+            }
+        );
+        
+        panel.setVisible(true);
         panel.setBackground(Color.BLUE); // make panel background color blue
         panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50)); // add panel padding for more empty space
 
         frame.setResizable(false); // make the frame non-resizable, therefore responsive
 
-
-        //My attempt at this
         int[][] userBoard2 = userBoard.getBoard();
 
         for (int row = 0; row < GRID_SIZE; row++) 
@@ -73,69 +104,29 @@ public class SudokuUI extends Sudoku implements EventListener
                             int tempCol = tempNode.getCol();
                             UIBoard[tempRow][tempCol] = tempNode;
                             
-                            /* 
-                            int[][] tempSourceBoard = new int[9][9];
-                            //copy2DArray(sourceBoard.getBoard(), tempSourceBoard);
-                           System.out.println("row" + tempRow);
-                           System.out.println("col" + tempCol);
-                            
-                            System.out.println("row col: " + sourceBoard.getValue(tempRow, tempCol));
-                            
-                            if(tempNode.getValue() == sourceBoard.getValue(tempRow, tempCol))
-                            {
-                                UIBoard[tempRow][tempCol] = tempNode;
-                            }
-                            else
-                            {
-                                textField.setText("");
-                                //TODO a JFrame popup saying wrong input
-                                //TODO Highlight the row and column
-                                //TODO highlight the other numbers that are the same
-                                System.out.println("WRONG INPUT");
-                            }
-*/
-                            
+              
 
                         System.out.println("Text=" + textField.getText());
                         }
                     });
                         
                     textField.setHorizontalAlignment(JTextField.CENTER);
-                    // Make bold lines to separate the 9 3x3 grids
-                    /* 
-                    textField.setBorder(BorderFactory.createMatteBorder(
-                            i / 9 == 0 ? 3 : 1, // top border
-                            i % 9 == 0 ? 3 : 1, // left border
-                            (i / 9 + 1) % 3 == 0 ? 3 : 1, // bottom border
-                            (i % 9 + 1) % 3 == 0 ? 3 : 1, // right border
-                            Color.BLACK));
-*/
-                    gridPanel.add(textField);
 
-                    // if cell not == 0, create a label for the number value
+                    gridPanel.add(textField);
                 }
                 else 
                 {
 
                     JLabel numberLabel = new JLabel(Integer.toString(value));
-                      //Cast the obj to CellNode
-                      CellNode tempNode = new CellNode(row, col);
-                      tempNode.setValue(Integer.toString(value));
-                      int tempRow = tempNode.getRow();
-                      int tempCol = tempNode.getCol();
+                        //Cast the obj to CellNode
+                        CellNode tempNode = new CellNode(row, col);
+                        tempNode.setValue(Integer.toString(value));
+                        int tempRow = tempNode.getRow();
+                        int tempCol = tempNode.getCol();
 
-                      UIBoard[tempRow][tempCol] = tempNode;
+                        UIBoard[tempRow][tempCol] = tempNode;
 
                     numberLabel.setHorizontalAlignment(JTextField.CENTER);
-                    // Make bold lines to separate the 9 3x3 grids
-                    /* 
-                    numberLabel.setBorder(BorderFactory.createMatteBorder(
-                            i / 9 == 0 ? 3 : 1, // top border
-                            i % 9 == 0 ? 3 : 1, // left border
-                            (i / 9 + 1) % 3 == 0 ? 3 : 1, // bottom border
-                            (i % 9 + 1) % 3 == 0 ? 3 : 1, // right border
-                            Color.BLACK));
-*/
                     gridPanel.add(numberLabel);
                 }
                 
@@ -144,152 +135,56 @@ public class SudokuUI extends Sudoku implements EventListener
         }
 
             System.out.println("Sum of all values: " + sum);
-/* 
-        // Create and add the text fields to the grid panel
-        for (int i = 0; i < 81; i++) 
-        {
 
+            
+        JLabel winLabel = new JLabel("Game in progress...");
+        
+        winningPanel.add(winLabel, BorderLayout.SOUTH);
+        winLabel.setHorizontalAlignment(JTextField.CENTER);
+        winLabel.setForeground(Color.WHITE);
+        winLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-            // if cell == 0, create a textfield
-            if(unsolvedBoardOneDimensional[i] == 0) 
-            {
-                JTextField textField = new CellNode(row, col);
-                textField.addActionListener(new ActionListener() 
-                {
-                    public void actionPerformed(ActionEvent e)
-                    {
-                      //getInput(e.getSource());
-                      System.out.println("Text=" + textField.getText());
-                    }
-                });
-                    
-                textField.setHorizontalAlignment(JTextField.CENTER);
-                // Make bold lines to separate the 9 3x3 grids
-                textField.setBorder(BorderFactory.createMatteBorder(
-                        i / 9 == 0 ? 3 : 1, // top border
-                        i % 9 == 0 ? 3 : 1, // left border
-                        (i / 9 + 1) % 3 == 0 ? 3 : 1, // bottom border
-                        (i % 9 + 1) % 3 == 0 ? 3 : 1, // right border
-                        Color.BLACK));
-
-                gridPanel.add(textField);
-
-                // if cell not == 0, create a label for the number value
-            }
-            else 
-            {
-                JLabel numberLabel = new JLabel(Integer.toString(unsolvedBoardOneDimensional[i]));
-                numberLabel.setHorizontalAlignment(JTextField.CENTER);
-                // Make bold lines to separate the 9 3x3 grids
-                numberLabel.setBorder(BorderFactory.createMatteBorder(
-                        i / 9 == 0 ? 3 : 1, // top border
-                        i % 9 == 0 ? 3 : 1, // left border
-                        (i / 9 + 1) % 3 == 0 ? 3 : 1, // bottom border
-                        (i % 9 + 1) % 3 == 0 ? 3 : 1, // right border
-                        Color.BLACK));
-
-                gridPanel.add(numberLabel);
-            }
-
-
-        }
-*/
+        winLabel.setVisible(true);
+        winLabel.setBackground(Color.BLUE);
 
         // Add the grid panel to the center of the main panel
         panel.add(gridPanel, BorderLayout.CENTER);
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
+        frame.setSize(900, 900);
         frame.setVisible(true);
 
-        // Add three buttons for selecting the difficulty level
-        JButton easyButton = new JButton("Easy");
-        JButton mediumButton = new JButton("Medium");
-        JButton hardButton = new JButton("Hard");
-
-        buttonPanel.add(easyButton);
-        buttonPanel.add(mediumButton);
-        buttonPanel.add(hardButton);
-
-
-        easyButton.addActionListener(new ActionListener()
-                                     {
-                                         public void actionPerformed(ActionEvent e)
-                                         {
-                                             System.out.println("Easy");
-                                         }
-                                     }
-        );
-
-        mediumButton.addActionListener(new ActionListener()
-                                       {
-                                           public void actionPerformed(ActionEvent e)
-                                           {
-                                               System.out.println("Medium");
-                                           }
-                                       }
-        );
-
-        hardButton.addActionListener(new ActionListener()
-                                     {
-                                         public void actionPerformed(ActionEvent e)
-                                         {
-                                             System.out.println("Hard");
-                                         }
-                                     }
-        );
-
         frame.add(buttonPanel, BorderLayout.NORTH); // Display the buttonPanel at the top of the frame
+        frame.add(winningPanel, BorderLayout.SOUTH); // Display the buttonPanel at the top of the frame
 
         // Set the background color of the button panel
         buttonPanel.setBackground(Color.BLUE);
+        winningPanel.setBackground(Color.BLUE);
+
 
         frame.add(panel);
-        frame.setSize(500, 500);
+        frame.setSize(600, 600);
         frame.setVisible(true);
         
 
-        
-
         int newSum = 0;
-        while(newSum != 405)
-        {
-
-            for(int row = 0; row < 9; row++)
-            {
-                for(int col = 0; col < 9; col++)
-                {
+        while(newSum != 405) {
+            for(int row = 0; row < 9; row++) {
+                for(int col = 0; col < 9; col++) {
                     CellNode temp = UIBoard[row][col];
-
                     try {
-                    
                         newSum += temp.getValue();
-                    } catch (NullPointerException e) 
-                    {
+                    } catch (NullPointerException e) {
                         continue;
-                    }
-
-                  
-                    
+                    }  
                 }
             }
-
             if(newSum != 405)
             {
                 newSum = 0;
             }
-            
-
-
-
-
         }
-
-        System.out.println("Done with Game u win");
-
-        
-
-    
-
+        System.out.println("You win!");
+        winLabel.setText("You win!");
     }
 }
