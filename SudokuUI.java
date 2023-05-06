e83199dde22383d86162cdbf4f88d95b14402dab
PrabhnoorKhatkar <prabhnoorkhatkar@gmail.com>
2023-05-06 02:11:32 -0700

    generateUi added

--- SudokuUI.java
+++ SudokuUI.java
@@ -11,30 +11,8 @@ public class SudokuUI extends Sudoku implements EventListener
     private static CellNode[][] UIBoard = new CellNode[9][9];
     private static int[][] sourceBoard2D = new int[9][9]; 
 
-
-    public void generateUI()
+    public static void generateUI(int diffculty,  JPanel gridPanel, JPanel panel, JFrame frame)
     {
-
-    }
-
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame("Sudoku Grid");
-        JPanel panel = new JPanel(new BorderLayout());
-        JPanel gridPanel = new JPanel(new GridLayout(9, 9));
-        JPanel buttonPanel = new JPanel(new FlowLayout());
-        JPanel winningPanel = new JPanel(new FlowLayout());
-        
-        // Buttons
-        JButton easyButton = new JButton("Easy");
-        JButton mediumButton = new JButton("Medium");
-        JButton hardButton = new JButton("Hard");
-
-        buttonPanel.add(easyButton);
-        buttonPanel.add(mediumButton);
-        buttonPanel.add(hardButton);
-        
-        
         Sudoku userBoard = new Sudoku();
         Sudoku sourceBoard = new Sudoku();
 
@@ -49,49 +27,10 @@ public class SudokuUI extends Sudoku implements EventListener
 
         userBoard.copyBoard(sourceBoard);
 
-        userBoard.removeNumbers(1);
+        userBoard.removeNumbers(diffculty);
 
 
-        easyButton.addActionListener(new ActionListener()
-            {
-                public void actionPerformed(ActionEvent e)
-                {
-                    System.out.println("Easy");
-
-                }
-            }
-        );
-
-        mediumButton.addActionListener(new ActionListener()
-            {
-                public void actionPerformed(ActionEvent e)
-                {
-                    System.out.println("Medium");
-                }
-            }
-        );
-
-        hardButton.addActionListener(new ActionListener()
-            {
-                public void actionPerformed(ActionEvent e)
-                {
-                    System.out.println("Hard");
-
-                }
-            }
-        );
-        
-        
-        JLabel winLabel = new JLabel("Game in progress...");
-        
-        
-        panel.setVisible(true);
-        panel.setBackground(Color.BLUE); // make panel background color blue
-        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50)); // add panel padding for more empty space
-
-        frame.setResizable(false); // make the frame non-resizable, therefore responsive
-
-        int[][] userBoard2 = userBoard.getBoard();
+         int[][] userBoard2 = userBoard.getBoard();
 
         for (int row = 0; row < GRID_SIZE; row++) 
         {
@@ -122,7 +61,7 @@ public class SudokuUI extends Sudoku implements EventListener
                             else
                             {    
                                 textField.setText("");
-                                winLabel.setText("Wrong Input!");
+                                //TODO a JFrame popup saying wrong input
                                 //TODO Highlight the row and column
                                 //TODO highlight the other numbers that are the same
                                 System.out.println("WRONG INPUT");
@@ -156,8 +95,42 @@ public class SudokuUI extends Sudoku implements EventListener
 
             }
         }
-            
+
+        panel.add(gridPanel, BorderLayout.CENTER);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900, 900);
+        frame.setVisible(true);
+
+
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Sudoku Grid");
+        JPanel panel = new JPanel(new BorderLayout());
+        JPanel gridPanel = new JPanel(new GridLayout(9, 9));
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        JPanel winningPanel = new JPanel(new FlowLayout());
         
+        // Buttons
+        JButton easyButton = new JButton("Easy");
+        JButton mediumButton = new JButton("Medium");
+        JButton hardButton = new JButton("Hard");
+
+        buttonPanel.add(easyButton);
+        buttonPanel.add(mediumButton);
+        buttonPanel.add(hardButton);
+    
+        
+        panel.setVisible(true);
+        panel.setBackground(Color.BLUE); // make panel background color blue
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50)); // add panel padding for more empty space
+
+        frame.setResizable(false); // make the frame non-resizable, therefore responsive
+
+       
+        JLabel winLabel = new JLabel("Game in progress...");
         
         winningPanel.add(winLabel, BorderLayout.SOUTH);
         winLabel.setHorizontalAlignment(JTextField.CENTER);
@@ -174,6 +147,8 @@ public class SudokuUI extends Sudoku implements EventListener
         frame.setSize(900, 900);
         frame.setVisible(true);
 
+        
+
         frame.add(buttonPanel, BorderLayout.NORTH); // Display the buttonPanel at the top of the frame
         frame.add(winningPanel, BorderLayout.SOUTH); // Display the buttonPanel at the top of the frame
 
@@ -185,6 +160,42 @@ public class SudokuUI extends Sudoku implements EventListener
         frame.add(panel);
         frame.setSize(600, 600);
         frame.setVisible(true);
+
+
+
+
+         easyButton.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    System.out.println("Easy");
+                    generateUI(10, gridPanel, panel, frame);
+
+
+                }
+            }
+        );
+
+        mediumButton.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    System.out.println("Medium");
+                    generateUI(30, gridPanel, panel, frame);
+                }
+            }
+        );
+
+        hardButton.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    System.out.println("Hard");
+                    generateUI(54, gridPanel, panel, frame);
+
+                }
+            }
+        );
         
 
         int newSum = 0;
