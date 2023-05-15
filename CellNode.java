11487d73bf4c074823491b78a66933b5bdcbc2db
PrabhnoorKhatkar <prabhnoorkhatkar@gmail.com>
2023-05-14 20:15:52 -0700

    document CellNode

--- CellNode.java
+++ CellNode.java
@@ -2,23 +2,38 @@ import javax.swing.JTextField;
 
 public class CellNode extends JTextField
 {
-
+    //Instance Variables
     private int row;
     private int col;
     private int value;
 
+    /*
+     * Getter
+     * @return row 1-9
+     */
     public int getRow() {
         return row;
     }
-
+    /*
+     * Getter
+     * @return col 1-9
+     */
     public int getCol() {
         return col;
     }
 
+    /*
+     * Getter
+     * @return value an int 1-9
+     */
     public int getValue() {
         return value;
     }
 
+    /*
+     * Setter
+     * @param value to be set to in integer value
+     */
     public void setValue(String value) 
     {
         this.value = Integer.parseInt(value);
@@ -26,6 +41,11 @@ public class CellNode extends JTextField
     }
 
 
+    /*
+     * Constructor
+     * @param row at which cellNode is desginated to
+     * @param col at which cellNode is desginated to
+     */
     public CellNode(int row, int col)
     {
         
